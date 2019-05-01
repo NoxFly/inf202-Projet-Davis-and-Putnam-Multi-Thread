73ec5a7cf92fdc9827a83bba6b563fa272834bf5
NoxFly <dorian.thivolle@gmail.com>
2019-05-02 01:43:56 +0200

    Davis and Putnam algorithm added. Missing something... Log function created

--- projet2/projet2.py
+++ projet2/projet2.py
@@ -2,7 +2,7 @@
 # Dorian THIVOLLE
 
 # imports
-import sys, os, re
+import sys, os, datetime
 from random import randint, randrange
 
 # vars
@@ -10,11 +10,12 @@ argc = len(sys.argv) - 1
 args = sys.argv[1:]
 
 # CONSTS
-NB_CLAUSES = 2
-MAX_NUM_ATOM = 6
-MAX_LG_CLAUSE = 5
+NB_CLAUSES = 20
+MAX_NUM_ATOM = 26
+MAX_LG_CLAUSE = 10
 WRITE_TYPE = 0
-
+NIV_TRACE = 2
+LOG = []
 
 # verification of given arguments
 def verifyInput():
@@ -22,8 +23,7 @@ def verifyInput():
         print("2 arguments expected.",argc,"given")
         return 1
 
-    pattern = re.compile("[\w\/\\\_\-]+\.txt")
-    if not pattern.match(args[0]):
+    if not args[0].endswith('.txt'):
         print("First argument must be file name with .txt extension")
         return 2
     
@@ -74,15 +74,21 @@ def printArray(arr):
         for i in arr: print("  "+str(i).replace(",",""))
         print("]")
 
-def ecrireEnsClauses(ensClauses, convSor):
-    # and / or
-    if convSor == 0: clauseSeparator, elementSeparator, brackets = " and ", " or ", ["(",")"]
-    # [[][]]
-    elif convSor == 1: clauseSeparator, elementSeparator, brackets = " ", " ", ["[","]"]
-    # +- | default
-    else: clauseSeparator, elementSeparator, brackets = " + ", "", ["",""]
 
-    ensClauses = clauseSeparator.join(map(str, parseConv2(ensClauses))).replace('[',brackets[0]).replace(']',brackets[1]).replace(',',elementSeparator)
+def stringify(arr):
+    sArr = '['
+    for i in arr:
+        sArr += '\n\t'+str(i)
+    sArr += '\n]'
+    return sArr
+
+
+def ecrireEnsClauses(ensClauses, convSor):
+    if convSor == 0:        clauseSeparator, elementSeparator, brackets = " and ",  " or", ["(",")"]
+    elif convSor == 1:      clauseSeparator, elementSeparator, brackets = " ",      " ",    ["[","]"]
+    else:                   clauseSeparator, elementSeparator, brackets = " + ",    "",     ["",""]
+
+    ensClauses = clauseSeparator.join(map(str, parseConv2(ensClauses))).replace('[',brackets[0]).replace(']',brackets[1]).replace(',',elementSeparator).replace("'",'')
     if convSor == 1: ensClauses = "[ "+ensClauses+" ]"
     print(ensClauses)
 
@@ -133,15 +139,156 @@ def internalRepr(matrix):
 
 # return internal representation of file clauses
 def lireEnsClauses(fichEntree, convEnt):
-    return internalRepr(getArray(fichEntree, convEnt))
+    ensClauses = getArray(fichEntree, convEnt)
+    #printArray(ensClauses)
+    #algoDP(ensClauses, NIV_TRACE)
+    return internalRepr(ensClauses)
 
 # random generation of clauses
 def genAleaEnsClauses(nbClauses, maxNumAtomes, maxLgClauses):
     matrix = [[randint(1,26) if randrange(-1,2,2) == 1 else -randint(1,26) for j in range(maxLgClauses)] for i in range(nbClauses)]
     return parseConv1(matrix) if args[1] == 1 else parseConv2(matrix)
 
+# write new log file
+def writeLog(content):
+    path = "log"
+    if not os.path.isdir(path): os.mkdir(path)
+    nFiles = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))]) + 1
+    nameFile = "log_"+str(nFiles)+"_trace_"+str(NIV_TRACE)+".txt"
+    print("Log file created : "+path+"/"+nameFile)
+
+    fd = open(path+"/"+nameFile, "w")
+    fd.write("\n".join(LOG))
+    fd.close()
 
 
+# append log message in array of log
+def log(msg=''):
+    LOG.append(msg)
+
+def removeArray(arr, arr2):
+    for i in arr2:
+        if i in arr: arr.remove(i)
+
+# Davis and Putnam
+def sat(ensClauses, verbose):
+    F = ensClauses[:]
+    log('\n\n'+('-'*50)+' F :\n'+str(F if args[1]==1 else parseConv2(F)))
+        
+    # 1.
+    if len(F) == 0:
+        if verbose: log('\nSATISFAISABLE')
+        return True
+    Lit1, Atomes, U = [], [], []
+
+    # 2.
+    i = 0
+    for k in range(len(F)):
+        if len(F[k-i]) == 0:
+            if verbose: log("La clause "+str(k)+" est vide: NON SATISFAISABLE")
+            return False
+        C = F[k-i][:]
+        if len(C) == 1:
+            U.extend(C)
+            F.remove(C)
+            i += 1
+            if verbose: log("La clause "+str(k)+" n'a qu'un litteral: ajout du litteral dans U et suppression dans F")
+        for j in C:
+            if not j in Lit1: Lit1.append(j)
+            if not abs(j) in Atomes: Atomes.append(abs(j))
+    
+    if verbose: log("F apres la premiere etape: "+str(F)+"\nU: "+str(U)+"\nAtomes: "+str(Atomes)+"\nLit1: "+str(Lit1))
+
+    # 3.
+    for i in U:
+        if -i in U:
+            if verbose: log("Opposé de "+str(i)+" trouve dans U: contradiction: NON SATISFAISABLE")
+            return False
+    
+    for k in range(len(F)):
+        C = F[k][:]
+        if any(lit in U for lit in C):
+            if verbose: log("litteral dans la clause C "+str(k)+" trouve dans U: NON SATISFAISABLE")
+            return False
+    
+    removeArray(Lit1, U)
+    removeArray(Atomes, [abs(a) for a in U])
+    if verbose:
+        log("Suppression de tous les litteraux de U dans Lit1: "+str(Lit1))
+        log("Suppression de tous les atomes de U dans Atomes: "+str(Atomes))
+
+    # 4.
+    if len(Lit1) > 0:
+        i = 0
+        for k in range(len(F)):
+            C = F[k-i][:]
+            if any(lit in Lit1 for lit in C):
+                if verbose: log("litteral dans la clause C "+str(k)+" trouve dans Lit1: suppression de la clause C dans F")
+                F.remove(C)
+                i += 1
+        removeArray(Atomes, [a for a in Lit1 if a >= 0])
+        if verbose: log("Suppression de tous les atomes de Lit1 dans Atomes: "+str(Atomes)+"\nF: "+str(F))
+
+    # 5.
+    for i in range(len(F)):
+        C = F[:]
+        del C[i]
+        for j in range(len(C)):
+            if set(F[i]).issubset(C[j]): removeArray(C[j], F[i])
+    
+    if verbose: log('Phase omettable: F: '+str(F))
+    
+
+    # 6.
+    if F != ensClauses: return sat(F, verbose)
+
+    # 7.
+    Fp, Fm = [], []
+    if verbose: log('Construction de F+ et F-')
+    for a in Atomes:
+        i = 0
+        for k in range(len(F)):
+            C = F[k-i][:]
+            if a in C:
+                if verbose: log("Atome "+str(a)+" trouve dans C "+str(k)+": suppression de C dans F et ajout de l'atome dans F+")
+                F.remove(C)
+                Fp.append(C.remove(a))
+                i += 1
+            if -a in C:
+                if verbose: log("inverse de l'Atome "+str(a)+" trouve dans C "+str(k)+": suppression de C dans F et ajout de l'inverse de l'atome dans F-")
+                F.remove(C)
+                Fm.append(C.remove(a))
+                i += 1
+    
+    resaF = []
+    for C in Fp:
+        for D in Fm:
+            n = C or D
+            if not n in resaF: resaF.append(n)
+
+    for i in resaF:
+        if not i in F: F.append(i)
+    
+    if verbose:
+        log('resa(F): '+str(resaF)+'\nUnion de resa(F) et F: '+str(F))
+
+    # 8.
+    return sat(F, verbose)
+
+
+# function that init then call sat
+def algoDP(ensClauses, nivTraces):
+    LOG = []
+    log('LOG Davis & Putnam - '+str(datetime.datetime.now())+'\nEnsemble de clauses :\n'+stringify(ensClauses))
+    if sat(parseConv1(ensClauses), nivTraces==2): print("satisfaisable")
+    else: print("non satisfaisable")
+    if(nivTraces): writeLog(LOG)
+
+
+
+
+
+############
 # execution
 
 if verifyInput(): exit(1)
@@ -152,6 +299,7 @@ printInternalRepr(ensClauses)
 
 # 2.3 - Génération aléatoire d'ensemble de clauses
 ensClauses = genAleaEnsClauses(NB_CLAUSES, MAX_NUM_ATOM, MAX_LG_CLAUSE)
-print(ensClauses)
+
 # 2.4 - Ecriture
-#ecrireEnsClauses(ensClauses, WRITE_TYPE)
+ecrireEnsClauses(ensClauses, WRITE_TYPE)
+algoDP(ensClauses, NIV_TRACE)
